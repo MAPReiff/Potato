a6e0cd1dd805e9e78944c8e86ec7c64c2c477ce4
Mitchell Reiff <mapr323@gmail.com>
2017-03-25 16:45:55 -0400

    Create PotatoFact.java
    Created a file detailing why a potato is more genetically advanced than a human.

---
+++ src/main/java/org/drtshock/PotatoFact.java
@@ -0,0 +1,8 @@
+package org.drtshock;
+
+public class PotatoFact {
+
+  //Prints out the best fact about potatoes
+  System.out.println("Potatoes have 48 chromosomes! That is two more than humans!");
+    
+}
